a6e1325ff8a033e3d8825eff7aec0eecc6ae6500
noah bernardi <noah.bernardi12@gmail.com>
2025-11-19 17:31:29 +0100

    verification et nettoyage du projet

--- routes/books.js
+++ routes/books.js
@@ -6,12 +6,7 @@ const router = express.Router();
 
 
 
-router.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+
 
 
 router.post('/', auth, multer, booksCtrl.createBook);
